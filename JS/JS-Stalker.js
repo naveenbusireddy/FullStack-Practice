c02dab71c2f79afa10ff7ae0149cf521e9a3f22a
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-12-05 01:18:30 +0530

    initial commit

---
+++ JS/JS-Stalker.js
@@ -0,0 +1,7 @@
+const firstName = prompt("what's your first Name?")
+const lastName = prompt("what's your last Name?")
+const age = prompt("your Age ?")
+alert(`your full Name is `+firstName+" "+lastName +`, you are `+age +"years old")
+
+console.log(`your full Name is:` +firstName+" "+lastName +`, you are` +age +"years old");
+
