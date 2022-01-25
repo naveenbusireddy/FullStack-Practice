b9b7ebd7aca524e42ae66af9c12b90f46757169f
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-01-25 17:03:54 +0530

    generates random colors

--- JS/color-game.js
+++ JS/color-game.js
@@ -1,19 +1,45 @@
-const colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)"
-];
+// const colors = [
+//     "rgb(255, 0, 0)",
+//     "rgb(0, 255, 0)",
+//     "rgb(0, 0, 255)",
+//     "rgb(255, 255, 0)",
+//     "rgb(255, 0, 255)",
+//     "rgb(0, 255, 255)"
+// ];
+const changeColor = (color) => {
+    squares.forEach((square) => {
+        square.style.backgroundColor = color;
+    })
+}
+
+//Generate random color
+const generateRandomColor = () => {
+    const g = Math.floor(Math.random() * 256);
+    const r = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const generateRandomColors = (num) => {
+    let output = [];
+    for(let i=0; i<num; i++)
+    {
+        output.push(generateRandomColor())
+    }
+    return output;
+}
+colors = generateRandomColors(6);
 
 //select elements
 const squares = document.querySelectorAll(".square")
 const colorDisplay = document.getElementById("colorDisplay");
 const message = document.getElementById("message");
+const title = document.querySelector("h1");
+const body = document.querySelector("body");
+
 
 const pickColor = () => {
-    const random = Math.floor(Math.random()*6);
+    const random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
 
@@ -36,16 +62,12 @@ for(let i=0; i<squares.length; i++)
         {
             message.textContent = "Correct !";
             changeColor(pickedColor);
+            title.style.backgroundColor = pickedColor;
         }
         else {
             message.textContent = "Wrong !";
-            this.style.backgroundColor = "black";
+            this.style.backgroundColor = `${body.style.backgroundColor}`;
         }
     })
 }
 
-const changeColor = (color) => {
-    squares.forEach((square) => {
-        square.style.backgroundColor = color;
-    })
-}
